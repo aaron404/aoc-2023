2106fa6b79e59e5405557a3b0c07b7d3eb9f07f5
Aaron Hill <aaronhill404@gmail.com>
2023-12-01 10:24:44 -0500

    day 1

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+const NUMBERS: &[&str] = &[
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn line_to_num(line: &str) -> u32 {
+    let digits = line
+        .chars()
+        .filter(|c| "0123456789".contains(*c))
+        .collect::<Vec<char>>();
+    let first = digits.first().unwrap().to_string().parse::<u32>().unwrap();
+    let last = digits.last().unwrap().to_string().parse::<u32>().unwrap();
+    first * 10 + last
+}
+
+fn line_to_sum2(line: &str) -> u32 {
+    let mut finds = Vec::new();
+
+    for i in 0..line.len() {
+        if let Ok(n) = line.to_string()[i..i + 1].parse::<u32>() {
+            finds.push(n)
+        } else {
+            for (j, num) in NUMBERS.iter().enumerate() {
+                if line[i..].starts_with(num) {
+                    finds.push(j as u32);
+                }
+            }
+        }
+    }
+    let first = finds.first().unwrap();
+    let last = finds.last().unwrap();
+    first * 10 + last
+}
+
+fn main() {
+    let input = include_str!("input/input1.txt");
+    let result: u32 = input.lines().map(line_to_num).sum();
+    let result2: u32 = input.lines().map(line_to_sum2).sum();
+    println!("{result}");
+    println!("{result2}");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
