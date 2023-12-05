f9501ef928e7b04637ab5881779bf840c81c5a1e
Aaron Hill <aaronhill404@gmail.com>
2023-12-05 01:02:51 -0500

    day 5

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![feature(ascii_char)]
 #![feature(ascii_char_variants)]
 
-use std::{collections::HashSet, str::Bytes};
+use std::collections::HashSet;
 
 const NUMBERS: &[&str] = &[
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
@@ -286,16 +286,132 @@ fn day4() {
     println!("day 4 part2 count: {}", counts.into_iter().sum::<u32>());
 }
 
-fn main() {
-    let input = include_str!("input/input1.txt");
-    let result: u32 = input.lines().map(line_to_num).sum();
-    let result2: u32 = input.lines().map(line_to_sum2).sum();
-    println!("{result}");
-    println!("{result2}");
+#[derive(Debug)]
+struct Range {
+    src: u64,
+    dst: u64,
+    len: u64,
+}
 
-    day2();
-    day3();
-    day4();
+impl Range {
+    fn map(&self, num: u64) -> Option<u64> {
+        if num >= self.src && num < self.src + self.len {
+            return Some(num - self.src + self.dst);
+        }
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Map {
+    ranges: Vec<Range>,
+}
+
+impl Map {
+    fn map(&self, num: u64) -> u64 {
+        for range in self.ranges.iter() {
+            if let Some(val) = range.map(num) {
+                return val;
+            }
+        }
+        num
+    }
+}
+
+fn day5() {
+    let input = include_str!("input/input5.txt");
+    let mut lines = input.lines();
+    let seeds = lines
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|num| num.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+
+    let mut maps: Vec<Map> = Vec::new();
+
+    lines.next(); // skip first empty line
+    lines.next(); // skip first map name line
+
+    let mut current_map = Map { ranges: Vec::new() };
+    let mut done = false;
+    while !done {
+        if let Some(line) = lines.next() {
+            if line.ends_with(':') {
+                maps.push(current_map);
+                current_map = Map { ranges: Vec::new() };
+            } else if line.is_empty() {
+            } else {
+                let nums = line
+                    .split_ascii_whitespace()
+                    .map(|num| num.parse::<u64>().unwrap())
+                    .collect::<Vec<u64>>();
+                current_map.ranges.push(Range {
+                    src: nums[1],
+                    dst: nums[0],
+                    len: nums[2],
+                });
+            }
+        } else {
+            done = true;
+        }
+    }
+    maps.push(current_map);
+
+    // println!("{seeds:?}");
+    // println!("{maps:?}");
+
+    let mut best_location = u64::MAX;
+    for seed in seeds.iter() {
+        let mut val = *seed;
+        // println!("mapping seed: {seed}");
+        for map in maps.iter() {
+            val = map.map(val);
+            // println!("  {val}");
+        }
+        if val < best_location {
+            best_location = val;
+        }
+    }
+    println!("day 5 part 1: {best_location}");
+
+    let mut best_location = u64::MAX;
+    for i in 0..seeds.len() / 2 {
+        println!("[{i}/{}]", seeds.len() / 2);
+        let start = seeds[i * 2];
+        let count = seeds[i * 2 + 1];
+
+        for seed in start..start + count {
+            let mut val = seed;
+            // println!("mapping seed: {seed}");
+            for map in maps.iter() {
+                val = map.map(val);
+                // println!("  {val}");
+            }
+            if val < best_location {
+                best_location = val;
+                println!("new best: {best_location}");
+            }
+        }
+    }
+
+    println!("day 5 part 2: {best_location}");
+}
+
+fn main() {
+    // let input = include_str!("input/input1.txt");
+    // let result: u32 = input.lines().map(line_to_num).sum();
+    // let result2: u32 = input.lines().map(line_to_sum2).sum();
+    // println!("{result}");
+    // println!("{result2}");
+
+    // day2();
+    // day3();
+    // day4();
+    day5();
 }
 
 #[cfg(test)]
